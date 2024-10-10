afe1da5fc3aafc2ee6a12c5ceabd90e727f74a38
2AW3 User 21 <2AW3-21@yourdomain.com>
2024-10-10 08:46:51 +0200

    10/10/2024

---
+++ destinos/nav.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active'); // Alternar la clase 'active'
+  });
