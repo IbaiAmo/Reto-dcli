350ffef651cb19907f93479335061165212c6738
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-10-14 00:18:37 +0200

    Página de actividades hecha

--- Ibai/scripts/nav.js
+++ Ibai/scripts/nav.js
@@ -2,13 +2,16 @@ const menuBtn = document.getElementById('menu-toggle');
 const closeBtn = document.getElementById('menu-close');
 const menu = document.querySelector('.links');
 const fondo = document.querySelector('.fondoNegro');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', function() {
   menu.classList.add('active');
   fondo.style.display = "block";
+  body.style.overflowY = "hidden";
 });
 
 closeBtn.addEventListener('click', function() {
   menu.classList.remove('active');
   fondo.style.display = "none";
+  body.style.overflowY = "auto";
 });
