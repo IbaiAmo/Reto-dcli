416198aaac85ae26143d994652d11953b922593f
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-10-14 22:26:05 +0200

    Proyecto estructurado, pero no acabado, falta poner mejor las carpetas

--- Turismo Japon/destinos/nav.js
+++ Turismo Japon/destinos/nav.js
@@ -1,4 +1,17 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active'); // Alternar la clase 'active'
-  });
+const menuBtn = document.getElementById('menu-toggle');
+const closeBtn = document.getElementById('menu-close');
+const menu = document.querySelector('.links');
+const fondo = document.querySelector('.fondoNegro');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click', function() {
+  menu.classList.add('active');
+  fondo.style.display = "block";
+  body.style.overflowY = "hidden";
+});
+
+closeBtn.addEventListener('click', function() {
+  menu.classList.remove('active');
+  fondo.style.display = "none";
+  body.style.overflowY = "auto";
+});
